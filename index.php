68b1e7ad7dbfaa4bb75264c0eb2eedec4c1d2ed6
dyoonar <71246827+dyoonar@users.noreply.github.com>
2020-10-07 10:18:41 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+$ag = "Rp. 13.000";
+$ab = "Rp. 15.000";
+$as = "Rp. 13.000";
+$tg = "Rp. 1.000";
+echo "<h1>Daftar Menu</h1>
+
+<table>
+    <tr>
+        <td><li>Ayam Goreng</li></td>
+        <td>=</td>
+        <td>".$ag."</td> 
+    </tr>
+    <tr>
+        <td><li>Ayam Bakar</li></td>
+        <td>=</td>
+        <td>".$ab."</td> 
+    </tr>
+    <tr>
+        <td><li>Ayam Sayur</li></td>
+        <td>=</td>
+        <td>Rp. 13.000</td> 
+    </tr>
+    <tr>
+        <td><li>Tempe Goreng</li></td>
+        <td>=</td>
+        <td>Rp. 1.000</td> 
+    </tr>
+    <tr>
+        <td><li>Tahu Goreng</li></td>
+        <td>=</td>
+        <td>Rp. 1.000</td> 
+    </tr>
+    <tr>
+        <td><li>Soto</li></td>
+        <td>=</td>
+        <td>Rp. 3.000</td> 
+    </tr>
+    <tr>
+        <td><li>Nasi Putih</li></td>
+        <td>=</td>
+        <td>Rp. 5.000</td> 
+    </tr>
+    <tr>
+        <td><li>Es Teh</li></td>
+        <td>=</td>
+        <td>Rp. 4.000</td> 
+    </tr>
+    <tr>
+        <td><li>Es Tebu</li></td>
+        <td>=</td>
+        <td>Rp. 5.000</td> 
+    </tr>
+
+</table>
+";
+
+?>
